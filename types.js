b8c4c6db9113a4e2b18fb7bcd748892c435a2699
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-01-25 20:02:33 +0600

    Learning data types

---
+++ types.js
@@ -0,0 +1,20 @@
+var name = "Programming Hero";
+// console.log(name);
+
+var address = "Palestine";
+// console.log(address);
+
+var country = "Bangladesh"
+console.log(country);
+// console.log(typeof country);
+
+var isRich = true;
+// console.log(isRich);
+
+var passed = false;
+// console.log(passed);
+// console.log(typeof passed);
+
+var price = 555;
+console.log(price);
+console.log(typeof price);
