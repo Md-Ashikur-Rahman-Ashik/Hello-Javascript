c48fd3b7533ce7ba88d874898ccbdd2084902f65
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-01-25 20:37:40 +0600

    Learning number in js

---
+++ number.js
@@ -0,0 +1,25 @@
+var orangePrice = 20;
+var chocolate = 0.50;
+// var applePrice = "20";
+// var applePrice = parseInt("20");
+// var applePrice = parseInt("Programming");
+// var applePrice = parseInt("22.5");
+var applePrice = parseFloat("22.5");
+
+// console.log(orangePrice + applePrice);
+
+// console.log(typeof orangePrice);
+
+// var applePrice = "26";
+// console.log(applePrice);
+// console.log(orangePrice);
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+// console.log(total);
+// console.log(total.toFixed);
+// console.log(total.toFixed(5));
+
+const num = 50 / 0;
+console.log(num);
