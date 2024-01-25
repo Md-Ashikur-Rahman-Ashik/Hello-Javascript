4d4f9d8474f5e612233bb234b6ac72c3d1fb7b69
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-01-25 21:01:38 +0600

    Learning javascript operations

---
+++ operation.js
@@ -0,0 +1,23 @@
+var eggPrice = 10;
+var onionPrice = 20;
+// console.log(eggPrice + onionPrice);
+
+// console.log(eggPrice - onionPrice);
+
+// console.log(onionPrice * eggPrice);
+
+// console.log(onionPrice / eggPrice);
+
+// console.log(10/3);
+
+// console.log(10%3);
+
+var money = 550;
+
+var price = 35;
+price = price + 10;
+// console.log(price);
+
+var newPrice = price + 50;
+var discountedPrice = newPrice - 10;
+console.log(discountedPrice);
