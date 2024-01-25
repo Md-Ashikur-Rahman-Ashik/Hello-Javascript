dc6929dce7a608b594a709c9688f055a40fb50a3
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-01-25 22:13:02 +0600

    Module finished with practice task

---
+++ mixedOp.js
@@ -0,0 +1,54 @@
+var first = "Programming"
+var second = "Hero"
+var fullName = first + second;
+
+// console.log(fullName);
+
+// console.log(40 - "20");
+
+// First Task
+
+// console.log("The amount of money:")
+var initialMoney = 1000;
+// console.log(initialMoney);
+
+// console.log("How much your food cost?")
+var foodCost = 700;
+// console.log(foodCost);
+
+// console.log("You will get back:")
+var finalMoney = initialMoney - foodCost;
+// console.log(finalMoney);
+
+// Second Task
+
+// console.log("The mark of all subjects are:");
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+// console.log(math, biology, chemistry, physics, bangla);
+
+// console.log("The average of all of them is:");
+var sum = math + biology + chemistry + physics + bangla;
+var average = sum / 5;
+// console.log(average.toFixed(2));
+
+// Third Task
+
+// console.log("The number is:");
+var number = 119;
+// console.log(number);
+
+// console.log("The remainder is:")
+var remainder = 119 % 5;
+// console.log(remainder);
+
+// Fourth Task
+
+const a = isNaN("11");
+console.log(a);
+
+const b = isNaN(2-10);
+console.log(b);
